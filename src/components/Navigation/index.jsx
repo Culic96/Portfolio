c0bd3f2cae01fe4aef694f4adaa42bc8161dc652
Nikola Culic <nikola.culic96@gmail.com>
2022-05-11 19:19:30 +0200

    Created Landing Page Layout

---
+++ src/components/Navigation/index.jsx
@@ -0,0 +1,21 @@
+ import './index.css';
+ const Navigation = () => {
+    return (
+        <>
+            <div className="nav-holder">
+                <div className="main-nav">
+                    <div className="nav-links">
+                        <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About me</a></li>
+                        <li><a href="#">Technologies</a></li>
+                        <li><a href="#">Contact</a></li>
+                        </ul> 
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navigation;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import Navigation from "../../components/Navigation";
+import "./index.css";
+const Home = () => {
+    return (
+        
+        <>
+        <Navigation/>
+        <div className="home-body">
+        <h2>Nikola Culic</h2>
+        <h2>HAHAHAH</h2>
+        </div>
+        </>
+    )
+}
+
+export default Home;
